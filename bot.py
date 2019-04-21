a0461f8f2468727c521de6372a3d2c1e23cb192d
Matt Morgan <Dozy-morgan@hotmail.co.uk>
2019-04-21 16:11:53 +0100

    Changed so different captains can be picked

--- bot.py
+++ bot.py
@@ -21,23 +21,25 @@ class Bot(discord.Client):
         specificNumbers = [0,1,2,3,4,5,6,7]
         names = message.content[9:].split(',')
         randomList = {}
-        if len(names) != 8:
-            await message.channel.send("There aren't enough people for cs, you need 8 (excluding captains) but you gave me " + str(len(names)))
+        if len(names) != 10:
+            await message.channel.send("There aren't enough people for cs, you need 10 names (First 2 captains) but you gave me " + str(len(names)-1))
             return
+        captains = [names[0], names[1]]
+        del names[0:2]
         for x in range(0, 8):
             number = random.choice(specificNumbers) # pick random number
             specificNumbers.remove(number) # remove number from list
             randomList[number] = names[x] # set name and random number
 
-        print(randomList)
+        # print(names) DEBUG MESSAGE 
 
-        adreans_team_string = "Team 1: \n 1: Adrian\n"
-        fins_team_string = "Team 2: \n 1: Fin\n"
+        team_1_string = "Team 1: \n 1: " + str(captains[0]) + "\n"
+        team_2_string = "Team 2: \n 1: " + str(captains[1]) + "\n"
         for x in range(0, 4):
-            adreans_team_string = adreans_team_string + str(x+2) + ": " + str(randomList.get(x)) + "\n"
-            fins_team_string = fins_team_string + str(x+2) + ": " + str(randomList.get(x+4)) + "\n"
+            team_1_string = team_1_string + str(x+2) + ": " + str(randomList.get(x)) + "\n"
+            team_2_string = team_2_string + str(x+2) + ": " + str(randomList.get(x+4)) + "\n"
 
-        await message.channel.send(adreans_team_string + "\n" + fins_team_string)
+        await message.channel.send(team_1_string + "\n" + team_2_string)
 
 loginFile= "settings/login.json"
 loginData = open(loginFile)
